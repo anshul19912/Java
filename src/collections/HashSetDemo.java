f4a8d3ae6acff443c8a98892317cf3b49fb343a8
anshul19912 <anshulsingh19912@gmail.com>
2021-12-09 13:16:20 +0530

    HashSet

---
+++ src/collections/HashSetDemo.java
@@ -0,0 +1,28 @@
+package collections;
+import java.util.HashSet;
+
+//hashset only stores unique value
+public class HashSetDemo {
+
+    public void demo(){
+
+        HashSet <String > course=new HashSet<>();
+
+        course.add("Java");
+        course.add("Java");            //it will ignore this
+        course.add("Spring");
+        course.add("Spring");          //it will ignore this
+        course.add("Spring Bot");
+        course.add("Cloud and Microservice ");
+
+        for(String var:course){
+            System.out.println(var);
+        }
+    }
+
+    public static void main(String[] args){
+
+        HashSetDemo obj=new HashSetDemo();
+        obj.demo();
+    }
+}
