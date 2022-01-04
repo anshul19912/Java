e352b789e7641f2782e93c41fc8a698027113863
anshul19912 <anshulsingh19912@gmail.com>
2022-01-04 13:05:10 +0530

    Exception_handling

---
+++ src/exception_handling/Demo.java
@@ -0,0 +1,25 @@
+package exception_handling;
+
+public class Demo {
+
+    public void divide(int a,int b){
+        int result=0;
+        try{
+            result=a/b;
+        }
+        catch (Exception obj){
+            System.out.println("Exception occured");
+        }
+
+        // finally block executes always whether there is exception or not
+        finally {
+            System.out.println("This is going to execute everytime.");
+        }
+        System.out.println("Result is "+result);
+    }
+
+    public static void main(String[] args){
+        Demo obj= new Demo();
+        obj.divide(2,2);
+    }
+}
